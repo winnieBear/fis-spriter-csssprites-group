b518e77c655119b0e4f31bd2dfddc9b0a6d96894
mudoo <mudoo@qq.com>
2016-12-12 19:03:52 +0800

    完善background-size:contain;识别

--- libs/css/rules.js
+++ libs/css/rules.js
@@ -9,7 +9,7 @@
 var Rules = Object.derive(function (id, css) {
     var self = this
         , _ = fis.util
-        , __size_re = /(?:\/\*[\s\S]*?(?:\*\/|$))|\b(?:width|height):([\s\S]*?)(?:\}|;|$)/
+        , __size_re = /(?:\/\*[\s\S]*?(?:\*\/|$))|\bwidth:\s*?([\d\.]+)(px|rem)\s*?(?:\}|;|$)|height:\s*?([\d\.]+)(px|rem)\s*?(?:\}|;|$)/g
         , __units_re = /\d+(px|rem)/i //尺寸单位
         , __background_re = /(?:\/\*[\s\S]*?(?:\*\/|$))|\bbackground(?:-image)?:([\s\S]*?)(?:\}|;|$)|background-position:([\s\S]*?)(?:\}|;|$)|(?:[^\{\};]*)background-repeat:([\s\S]*?)(?:\}|;|$)|(?:[^\{\};]*)background-size:([\s\S]*?)(?:\}|;|$)/gi
         , __image_url_re = /url\s*\(\s*("(?:[^\\"\r\n\f]|\\[\s\S])*"|'(?:[^\\'\n\r\f]|\\[\s\S])*'|[^)}]+)\s*\)/i
@@ -62,17 +62,23 @@ var Rules = Object.derive(function (id, css) {
         self._position[1] = res[2] === 'top' ? 0 : parseFloat(res[2]);
     }
 
-    function _get_units(res) {
-    	// console.log(res);
-    	if(!res[1]) return;
-
-    	var unitsRes = res[1].match(__units_re);
-    	// console.log(unitsRes[1]);
-    	if(unitsRes) {
-    		return unitsRes[1];
-    	}
-
-    	return 'px';
+    function _get_contain_size() {
+        var tmpUnit = ['', ''];
+        css.replace(__size_re, function(sizeM, width, widthUnit, height, heightUnit) {
+            if(width) {
+                self.size[0] = parseFloat(width);
+                tmpUnit[0] = widthUnit;
+            }
+            if(height) {
+                self.size[1] = parseFloat(height);
+                tmpUnit[1] = heightUnit;
+            }
+        });
+        if(tmpUnit[0]!='' && tmpUnit[0]==tmpUnit[1]) {
+            self.units = tmpUnit[0];
+        }else{
+            self.size = [-1, -1];
+        }
     }
 
     self._css = css.replace(__background_re,
@@ -116,7 +122,7 @@ var Rules = Object.derive(function (id, css) {
                 // color
                 res = image.match(__color_re);
                 if(res) {
-                	self.color = res[1].trim();
+                    self.color = res[1].trim();
                 }
             }
             if (position) {
@@ -141,11 +147,7 @@ var Rules = Object.derive(function (id, css) {
                     self.size[1] = parseFloat(res[3]);
                     self.units = res[2];
                 }else if(size.trim()=='contain') {
-                	res = css.match(__size_re);
-                	if(res) {
-                		self.units = _get_units(res);
-                		self.size = [0, 0];
-                	}
+                    _get_contain_size();
                 }
             }
             return __sprites_hook_ld + m + __sprites_hook_rd;
@@ -175,7 +177,7 @@ var Rules = Object.derive(function (id, css) {
                 ret += pre_pad + 'background-repeat: no-repeat;';
             }
             if(this.color) {
-            	ret += 'background-color:'+ this.color +';';
+                ret += 'background-color:'+ this.color +';';
             }
         }
         return ret;
